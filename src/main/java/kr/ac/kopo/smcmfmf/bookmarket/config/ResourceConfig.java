af6076c32fd2294ea056cba00e4afe497af9af5d
smcmfmf <hk0831bae@gmail.com>
2025-05-14 09:27:06 +0900

    BookMarket의 Chapter 7 파일 업로드 및 다운로드 수정

--- src/main/java/kr/ac/kopo/smcmfmf/bookmarket/config/ResourceConfig.java
+++ src/main/java/kr/ac/kopo/smcmfmf/bookmarket/config/ResourceConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ResourceConfig implements WebMvcConfigurer {
-    @Value("@{file.uploadDir}") // application.properties에 선언한 파일 저장 위치를 사용
+    @Value("${file.uploadDir}") // application.properties에 선언한 파일 저장 위치를 사용
     String fileDir; // 파일 경로 저장
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/BookMarket/images/**") // URI (상대 경로)
+        registry.addResourceHandler("/images/**") // URI (상대 경로) -> application.properties에서 context-path=/BookMarket가 선언되어 있으므로 하위 images부터 작성한다.
                 .addResourceLocations("file:///" + fileDir) // 윈도우 파일 경로
-                .setCachePeriod(60 * 60 * 24 * 365); // 접근하는 파일 캐싱 시간
+                .setCachePeriod(60 * 60 * 24 * 365); // 접근하는 파일 캐싱 시간 (초 단위) -> 1년
     }
 }
